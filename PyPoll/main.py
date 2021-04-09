d91aa00095030cc65ea9120e41ac48ba38eb71a5
Manny <hi@mannyparra.com>
2021-04-08 18:14:37 -0600

    add paths

---
+++ PyPoll/main.py
@@ -0,0 +1,11 @@
+#import modules
+import csv
+import os
+
+#define paths
+
+input_path = os.path.join('Resources', 'election_data.csv')
+output_path = os.path.join('Analysis', 'poll_results.txt')
+
+
+

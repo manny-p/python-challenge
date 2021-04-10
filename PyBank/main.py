1cfdef8bd129d0a2941dac8649aae753135e4dd6
Manny <hi@mannyparra.com>
2021-04-10 00:26:24 -0600

    calculate totals

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,35 +1,56 @@
+# import modules
 import csv
 import os
 import statistics
 
+# define paths
 filename = os.path.join('Resources', 'budget_data.csv')
-output = os.path.join('Analysis','output.txt')
+output = os.path.join('Analysis', 'output.txt')
 
+# read csv
 with open(filename) as f:
     reader = csv.reader(f, delimiter=',')
     skip_header = next(reader)
 
-    total_months = 0
-    total_net = 0
-    delta = 0
+    # initialize variables
+    total_months = []
+    net_profits_loss = []
+    delta_profits_loss = []
 
-    max_profit_date = "Jan-2010"
-    max_profit_amount = 0
+    # loop / append values to lists
+    for row in reader:
+        total_months.append(row[0])
+        net_profits_loss.append(int(row[1]))
 
-    max_loss_date = "Jan-2010"
-    max_loss_amount = 0
+# calculate total profits & losses
+total_profit_loss: int = 0
+for i in net_profits_loss:
+    total_profit_loss = i + total_profit_loss
 
-    list_date = []
-    list_net = []
+# use list comprehension to create new list
+# new_list = [expression for_loop_one_or_more conditions]
+delta_profits_loss = [net_profits_loss[i + 1] - net_profits_loss[i] for i in range(0, len(net_profits_loss) - 1)]
 
-    i = 0
+# calculate average
+delta_mean = round(statistics.mean(delta_profits_loss), 2)
 
-    for line in reader:
-        total_months += 1
-        total_net += int(line[1])
+# insert given element at given index (index, element)
+delta_profits_loss.insert(0, 0)
 
-        list_date.append(line[0])
-        list_net.append(line[1])
+# calculate max / min for profits/loss & dates
+max_profits_delta = max(delta_profits_loss)
+increase_date = total_months[delta_profits_loss.index(max_profits_delta)]
 
+min_profits_delta = min(delta_profits_loss)
+decrease_date = total_months[delta_profits_loss.index(min_profits_delta)]
+
+separator = '-------------------------'
+tm = f'Total Months: {len(total_months)}'
+tpl = f'Total: ${total_profit_loss:,}'
+dm = f'Average Change: ${delta_mean:,}'
+increase = f'Greatest Increase in Profits: {increase_date} (${max_profits_delta:,})'
+decrease = f'Greatest Decrease in Profits: {decrease_date} (${min_profits_delta:,})'
+
+print('Financial Analysis', separator, tm, tpl, dm, increase, decrease, sep='\n')
 
 
